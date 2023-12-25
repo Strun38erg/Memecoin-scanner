55ae124504c49eb90c5c716fbae10d0cf9450c2a
Struneberg <146861637+Struneberg@users.noreply.github.com>
2023-12-25 18:14:32 +0100

    Scans a given period of time and finds transactions over specified amount

--- index.js
+++ index.js
@@ -1,47 +1,67 @@
 require('dotenv').config();
 const Web3 = require('web3');
+const PepeCoinABI = require('./PepeCoinABI.json');
+const EthDater = require('ethereum-block-by-date');
+const abiDecoder = require('abi-decoder'); // Import abi-decoder
 
-console.log(`Connecting to Infura: ${process.env.INFURA_API_KEY}`);
-// Connect to the Ethereum node using the Infura API key from .env
-const web3 = new Web3(`https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`);
+const web3 = new Web3(new Web3.providers.HttpProvider(process.env.INFURA_API_KEY));
+const contractAddress = process.env.MEME_COIN_CONTRACT_ADDRESS.toLowerCase();
+const minAmount = web3.utils.toBN(process.env.MINIMUM_AMOUNT);
+const ethDater = new EthDater(web3);
 
-// Read the contract address, minimum Ether amount, and dates from .env
-const contractAddress = process.env.CONTRACT_ADDRESS;
-const minimumEtherAmount = web3.utils.toWei(process.env.MINIMUM_ETHER_AMOUNT, 'ether');
-const startDate = new Date(process.env.START_DATE).getTime() / 1000;
-const endDate = new Date(process.env.END_DATE).getTime() / 1000;
+const contract = new web3.eth.Contract(PepeCoinABI, contractAddress);
 
-// Function to scan transactions on the contract within a specific timeframe
-async function scanContractTransactionsInTimeframe() {
-  try {
-    // Get the latest block number
-    const latestBlockNumber = await web3.eth.getBlockNumber();
+// Initialize abi-decoder with your contract's ABI
+abiDecoder.addABI(PepeCoinABI);
 
-    // Loop through blocks within the specified timeframe
-    for (let blockNumber = latestBlockNumber; blockNumber >= 0; blockNumber--) {
-      const block = await web3.eth.getBlock(blockNumber, true);
+async function scanTransactions() {
+    const startDate = new Date('2023-04-01');
+    const endDate = new Date('2023-04-30');
 
-      // Check if the block timestamp is within the specified timeframe
-      if (block.timestamp >= startDate && block.timestamp <= endDate) {
+    const blocksInRange = await ethDater.getEvery(
+        'days',
+        startDate,
+        endDate,
+        1,
+        true,
+        false
+    );
+
+    for (const blockInfo of blocksInRange) {
+        console.log(`Scanning block ${blockInfo.block} - Date: ${blockInfo.date}`);
+        const block = await web3.eth.getBlock(blockInfo.block, true);
+
         for (const transaction of block.transactions) {
-          // Check if the transaction is to the contract and meets the minimum amount
-          if (transaction.to === contractAddress && transaction.value >= minimumEtherAmount) {
-            // Log the address that initiated (bought) in the transaction
-            console.log('Buying Address:', transaction.from);
-            console.log('Value:', web3.utils.fromWei(transaction.value, 'ether'), 'ETH');
-          }
-        }
-      }
+            if (transaction.to && transaction.to.toLowerCase() === contractAddress) {
+                try {
+                    const receipt = await web3.eth.getTransactionReceipt(transaction.hash);
+                    for (const log of receipt.logs) {
+                        if (log.address.toLowerCase() === contractAddress) {
+                            const decodedLogs = abiDecoder.decodeLogs([log]);
 
-      // Break the loop if we've gone before the specified timeframe
-      if (block.timestamp < startDate) {
-        break;
-      }
+                            if (decodedLogs && decodedLogs.length > 0) {
+                                const decodedLog = decodedLogs[0];
+                                const decodedValue = web3.utils.toBN(decodedLog.events[2].value);
+
+                                if (decodedValue.gte(minAmount)) {
+                                    console.log('Significant transaction found:', {
+                                        from: decodedLog.events[0].value,
+                                        to: decodedLog.events[1].value,
+                                        value: decodedValue.toString(),
+                                        transactionHash: transaction.hash
+                                    });
+                                }
+                            }
+                        }
+                    }
+                } catch (error) {
+                    console.error('Error processing transaction:', error);
+                }
+            }
+        }
     }
-  } catch (error) {
-    console.error('Error:', error);
-  }
+
+    console.log('Scanning complete.');
 }
 
-// Call the function to scan transactions
-scanContractTransactionsInTimeframe();
+scanTransactions().catch(console.error);
