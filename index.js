88c0ba737cda5485daaa344994182f347dbbe368
Struneberg <146861637+Struneberg@users.noreply.github.com>
2023-12-26 16:42:31 +0100

    new version

--- index.js
+++ index.js
@@ -1,67 +1,77 @@
-require('dotenv').config();
 const Web3 = require('web3');
-const PepeCoinABI = require('./PepeCoinABI.json');
-const EthDater = require('ethereum-block-by-date');
-const abiDecoder = require('abi-decoder'); // Import abi-decoder
+const abiDecoder = require('abi-decoder'); // Import ABI decoder
+const fs = require('fs');
 
-const web3 = new Web3(new Web3.providers.HttpProvider(process.env.INFURA_API_KEY));
-const contractAddress = process.env.MEME_COIN_CONTRACT_ADDRESS.toLowerCase();
-const minAmount = web3.utils.toBN(process.env.MINIMUM_AMOUNT);
-const ethDater = new EthDater(web3);
+const contractAddress = '0x6982508145454ce325ddbe47a25d4ec3d2311933'; // Your contract address
+const infuraApiKey = '8dadf78004a044cdb0184994fbc60984'; // Your Infura API key
+const pepeCoinABIPath = './PepeCoinABI.json'; // Path to your ABI file
+const maxTransactionCount = 10000; // Maximum transaction count
 
-const contract = new web3.eth.Contract(PepeCoinABI, contractAddress);
+// Initialize web3 with Infura
+const web3 = new Web3(new Web3.providers.HttpProvider(`https://mainnet.infura.io/v3/${infuraApiKey}`));
 
-// Initialize abi-decoder with your contract's ABI
-abiDecoder.addABI(PepeCoinABI);
+// Load and use ABI from file
+const pepeCoinABI = JSON.parse(fs.readFileSync(pepeCoinABIPath, 'utf8'));
+abiDecoder.addABI(pepeCoinABI);
 
-async function scanTransactions() {
-    const startDate = new Date('2023-04-01');
-    const endDate = new Date('2023-04-30');
+async function findBiggestTransfers() {
+    try {
+        const blockRangeStart = parseInt('17350899'); // Decimal start block
+        const blockRangeEnd = parseInt('17786898'); // Decimal end block
+        let transfers = [];
 
-    const blocksInRange = await ethDater.getEvery(
-        'days',
-        startDate,
-        endDate,
-        1,
-        true,
-        false
-    );
+        console.log(`Processing from block ${blockRangeStart} to ${blockRangeEnd}`);
 
-    for (const blockInfo of blocksInRange) {
-        console.log(`Scanning block ${blockInfo.block} - Date: ${blockInfo.date}`);
-        const block = await web3.eth.getBlock(blockInfo.block, true);
+        let fromBlock = blockRangeStart;
+        const batchSize = 1000; // Query in batches of 1,000 blocks
 
-        for (const transaction of block.transactions) {
-            if (transaction.to && transaction.to.toLowerCase() === contractAddress) {
-                try {
-                    const receipt = await web3.eth.getTransactionReceipt(transaction.hash);
-                    for (const log of receipt.logs) {
-                        if (log.address.toLowerCase() === contractAddress) {
-                            const decodedLogs = abiDecoder.decodeLogs([log]);
+        while (fromBlock <= blockRangeEnd) {
+            const toBlock = Math.min(fromBlock + batchSize - 1, blockRangeEnd);
+            console.log(`Fetching logs from block ${fromBlock} to ${toBlock}`);
 
-                            if (decodedLogs && decodedLogs.length > 0) {
-                                const decodedLog = decodedLogs[0];
-                                const decodedValue = web3.utils.toBN(decodedLog.events[2].value);
+            const logs = await web3.eth.getPastLogs({
+                fromBlock: web3.utils.toHex(fromBlock),
+                toBlock: web3.utils.toHex(toBlock),
+                address: contractAddress
+            });
 
-                                if (decodedValue.gte(minAmount)) {
-                                    console.log('Significant transaction found:', {
-                                        from: decodedLog.events[0].value,
-                                        to: decodedLog.events[1].value,
-                                        value: decodedValue.toString(),
-                                        transactionHash: transaction.hash
-                                    });
-                                }
-                            }
+            console.log(`Fetched ${logs.length} logs`);
+            
+            // Wrap the forEach loop in an async function
+            await Promise.all(logs.map(async (log) => {
+                const decodedLogs = abiDecoder.decodeLogs([log]);
+                for (const decodedLog of decodedLogs) {
+                    if (decodedLog && decodedLog.name === "Transfer") {
+                        const toAddress = decodedLog.events.find(e => e.name === 'to').value;
+                        
+                        // Check the transaction count of the address
+                        const transactionCount = await web3.eth.getTransactionCount(toAddress);
+                        
+                        if (transactionCount <= maxTransactionCount) {
+                            const valueInWei = decodedLog.events.find(e => e.name === 'value').value;
+                            const valueInEther = web3.utils.fromWei(valueInWei, 'ether');
+                            transfers.push({ address: toAddress, amount: valueInEther, txHash: log.transactionHash });
                         }
                     }
-                } catch (error) {
-                    console.error('Error processing transaction:', error);
                 }
-            }
-        }
-    }
+            }));
 
-    console.log('Scanning complete.');
+            fromBlock = toBlock + 1;
+        }
+
+        // Sort transfers by amount and get top 50
+        const topTransfers = transfers.sort((a, b) => parseFloat(b.amount) - parseFloat(a.amount)).slice(0, 50);
+
+        console.log("Top 50 transfers:");
+        topTransfers.forEach((transfer, index) => {
+            console.log(`#${index + 1}: ${transfer.amount} Ether to ${transfer.address} (Tx Hash: ${transfer.txHash})`);
+        });
+
+        console.log("Scan complete");
+    } catch (error) {
+        console.error('Error:', error);
+    }
 }
 
-scanTransactions().catch(console.error);
+findBiggestTransfers();
+
